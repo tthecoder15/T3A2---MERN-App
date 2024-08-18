f7be67b44acc54bb15cb819b6ecf528cb3735a37
Mike <michaellaishleys@gmail.com>
2024-08-18 10:49:53 +1000

    Added route files and routes to app.jsx

--- T3A2-B/Frontend/src/main.jsx
+++ T3A2-B/Frontend/src/main.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
@@ -7,4 +8,16 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+=======
+import react from 'react'
+import ReactDom from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+>>>>>>> frontend-setup
 )
