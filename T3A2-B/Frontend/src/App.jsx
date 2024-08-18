356be99b844d56a3bbe994530076a7b5057d45fe
Mike <michaellaishleys@gmail.com>
2024-08-18 12:26:55 +1000

    started building home, adding some css

--- T3A2-B/Frontend/src/App.jsx
+++ T3A2-B/Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import NavBar from './NavBar'
 import Home from './Home'
 import Team from './Team'
 import Contact from './Contact'
@@ -8,24 +9,26 @@ import Login from './Login'
 import MyAcc from './MyAcc'
 import Booking from './Booking'
 import Admin from './Admin'
+import Footer from './Footer'
 
 const App = () => {
   return (
     <>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/ourteam" element={<Team />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/user">
-        <Route path="/forgotpassword" element={<ForgotPass />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/myaccount" element={<MyAcc />} />
-      </Route>
-      <Route path="/booking" element={<Booking />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
-    <Footer />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ourteam" element={<Team />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/user">
+          <Route path="forgotpassword" element={<ForgotPass />} />
+          <Route path="login" element={<Login />} />
+          <Route path="myaccount" element={<MyAcc />} />
+        </Route>
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path='*' element={<h3>Page not found..</h3>} />
+      </Routes>
+      <Footer />
     </>
   )
 }
--- T3A2-B/Frontend/src/main.jsx
+++ T3A2-B/Frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import react from 'react'
-import ReactDom from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
