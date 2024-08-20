221bdb47fed9d5093d5dfea13c6ab4b76f71289e
Mike <michaellaishleys@gmail.com>
2024-08-20 13:31:05 +1000

    added images of vets and adjusted formatting to fit our team page.  Finished contact form, before styling.

--- T3A2-B/Frontend/src/components/Contact/ContactForm.jsx
+++ T3A2-B/Frontend/src/components/Contact/ContactForm.jsx
@@ -4,27 +4,43 @@ import { useForm, ValidationError } from '@formspree/react'
 function ContactForm() {
   const [state, handleSubmit] = useForm("xzzpareq")
   if (state.succeeded) {
-      return <p>Thank you for contacting.  
-        We will receive your enquiry shortly and be in touch as soon as possible.
-        </p>
+      return <p>Your message has been received.</p>
   }
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Full Name
       </label>
-      <input/>
+      <input
+        id="name"
+        type="text"
+        name="Full Name"
+      />
+      <ValidationError 
+        prefix="Name" 
+        field="name"
+        errors={state.errors}
+      />
       <label>
         Contact Number
       </label>
-      <input/>
+      <input
+        id="number"
+        type="text"
+        name="Contact Number"
+      />
+      <ValidationError 
+        prefix="Number" 
+        field="number"
+        errors={state.errors}
+      />
       <label htmlFor="email">
         Email Address
       </label>
       <input
         id="email"
         type="email" 
-        name="email"
+        name="Email"
       />
       <ValidationError 
         prefix="Email" 
@@ -34,13 +50,22 @@ function ContactForm() {
       <label>
         Pet Details
       </label>
-      <input/>
+      <input
+        id="pet"
+        type="text"
+        name="Pet Details"
+      />
+      <ValidationError 
+        prefix="Pet" 
+        field="pet"
+        errors={state.errors}
+      />
       <label>
         Your query
       </label>
       <textarea
         id="message"
-        name="message"
+        name="Message"
       />
       <ValidationError 
         prefix="Message" 
