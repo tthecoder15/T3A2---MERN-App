2cd89d0dd7cdf3e5ff77ea3f0f4304348d90e7df
Mike <michaellaishleys@gmail.com>
2024-08-20 11:14:01 +1000

    merge with frontend branch

--- T3A2-B/Frontend/src/components/NavBar.jsx
+++ T3A2-B/Frontend/src/components/NavBar.jsx
@@ -4,27 +4,21 @@ import { Link } from "react-router-dom"
 const NavBar = () => {
     return (
         <>
-            <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-                <div className="navbar-brand">
-                    <Link className="navbar-item" to="/">
+            <nav>
+                <div>
+                    <Link to="/">
                         <h1>---Insert PAWFECT CARE Icon---</h1>
                     </Link>
-                    <Link role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </Link>
                 </div>
-                <div id="navbarBasicExample" className="navbar-menu">
-                    <div className="navbar-start">
-                        <Link className="navbar-item" to="/">Home</Link>
-                        <Link className="navbar-item" to="/ourteam" >Our Team</Link>
-                        <Link className="navbar-item" to="/booking" >Book Now</Link>
-                        <Link className="navbar-item" to="/contact" >Contact Us</Link>
+                <div>
+                    <div>
+                        <Link to="/">Home</Link>
+                        <Link to="/ourteam" >Our Team</Link>
+                        <Link to="/booking" >Book Now</Link>
+                        <Link to="/contact" >Contact Us</Link>
                     </div>
-                    <div className="navbar-start">
-                        <Link className="navbar-item" to="/user/login" >Login/Register</Link>
+                    <div>
+                        <Link to="/user/login" >Login/Register</Link>
                     </div>
                 </div>
             </nav>
