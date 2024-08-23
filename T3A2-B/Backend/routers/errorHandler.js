9f992ec30af4b8cec286c6e75ee48c99bce37ece
tthecoder15 <ttutone15@gmail.com>
2024-08-23 14:33:18 +1000

    fixed up some error handling for unexpected errors

--- T3A2-B/Backend/routers/errorHandler.js
+++ T3A2-B/Backend/routers/errorHandler.js
@@ -2,29 +2,49 @@ import customErrors from "../errorObjs.js"
 
 function errorHandler (err, req, res, next) {
     let retErrObj
-    if ('path' in err) {
+
+    // Handles mongoose errors with multiple errors at once 
+    if ("errors" in err) { 
         let errKeys = Object.keys(err.errors)
         retErrObj = {"error/s": errKeys}
-        for (let err of errKeys) {
-            if (err.errors[err].reason) {
-                retErrObj[err.errors[err].path] = `${err.errors[err].reason}`
+        for (let err1 of errKeys) {
+            if (err.errors[err1].reason) {
+                retErrObj[err.errors[err1].path] = `${err.errors[err1].reason}`
             }
             else {
-                retErrObj[err.errors[err].path] = `${err.errors[err].message}`
+                retErrObj[err.errors[err1].path] = `${err.errors[err1].message}`
             }
         }
+        res.status(400).send(retErrObj)
     }
-    else if (err.code == 'credentials_required' || err.code == 'invalid_token') {
+
+    // Handles mongoose errors with only one error
+    else if ("path" in err) {
+        retErrObj = {"error/s": [err.name]}
+        retErrObj[err.name] = String(err.reason)
+        res.status(400).send(retErrObj)
+    }
+
+    // Handles JWT auth errors
+    else if (err.code == 'credentials_bad_format' || err.code == 'credentials_required' || err.code == 'invalid_token') {
         retErrObj = {"error/s": [err.code]}
         retErrObj[err.code] = err.inner.message
         res.status(400).send(retErrObj)
     }
+
+    // Handles custom errors I have coded, errors thrown are described in '../errorObjs.js'
     else {
-        console.log(err)
-        if ('code' in err) {
-            delete err.code
+        if (typeof(err.code) == 'number') {
+            retErrObj = {...err}
+            delete retErrObj.code
+            res.status(err.code).send(retErrObj.message)
         }
-        res.status(400).send(err)
+        else if (err.status) {
+            res.status(err.status).send(err)
+        }
+        else {
+            res.send(err).status(400)
+        }
     }
 }
 
