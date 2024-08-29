4076e7cdce9dcafc8b838e3171f9d9bfaace0f16
Mike <michaellaishleys@gmail.com>
2024-08-29 16:28:11 +1000

    added responsiveness to navbar and completed footer inc mobile responsiveness

--- T3A2-B/Frontend/src/components/NavBar.jsx
+++ T3A2-B/Frontend/src/components/NavBar.jsx
@@ -1,21 +1,31 @@
-import React from "react"
-import { Link, useNavigate } from "react-router-dom"
-import sessionState from "../routes/store"
-import './global.css'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import sessionState from "../routes/store";
+import './global.css';
 
 const NavBar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
   const isAuthenticated = sessionState((state) => state.isAuthenticated);
   const logout = sessionState((state) => state.logout);
   const navigate = useNavigate();
 
   const logoutClick = () => {
     logout();
-    navigate("/user/login")
+    navigate("/user/login");
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
   };
 
   return (
     <>
       <nav>
+        <div className="hamburger" onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
         <div className="navLogo">
           <Link to="/">
             <img
@@ -24,25 +34,25 @@ const NavBar = () => {
             />
           </Link>
         </div>
-          <div className="site-nav">
-            <Link to="/">Home</Link>
-            <p>|</p>
-            <Link to="/ourteam">Our Team</Link>
-            <p>|</p>
-            <Link to="/booking">Book Now</Link>
-            <p>|</p>
-            <Link to="/contact">Contact Us</Link>
-          </div>
-          <div className="login"> 
-              {isAuthenticated ? (
-                <>
-                  <Link id="logged-in" to="/user/login"><>My Account</></Link>
-                  <button id="logged-in" onClick={logoutClick}>Logout</button>
-                </>
-              ) : (
-                  <Link id="logged-in" to="/user/login">Login/Register</Link>
-              )}
-          </div>
+        <div className={`site-nav ${isMenuOpen ? 'active' : ''}`}>
+          <Link to="/" onClick={toggleMenu}>Home</Link>
+          <p>|</p>
+          <Link to="/ourteam" onClick={toggleMenu}>Our Team</Link>
+          <p>|</p>
+          <Link to="/booking" onClick={toggleMenu}>Book Now</Link>
+          <p>|</p>
+          <Link to="/contact" onClick={toggleMenu}>Contact Us</Link>
+        </div>
+        <div className={`login ${isMenuOpen ? 'active' : ''}`}>
+          {isAuthenticated ? (
+            <>
+              <Link id="logged-in" to="/user/login" onClick={toggleMenu}>My Account</Link>
+              <button id="logged-in" onClick={() => { logoutClick(); toggleMenu(); }}>Logout</button>
+            </>
+          ) : (
+            <Link id="logged-in" to="/user/login" onClick={toggleMenu}>Login/Register</Link>
+          )}
+        </div>
       </nav>
     </>
   );
