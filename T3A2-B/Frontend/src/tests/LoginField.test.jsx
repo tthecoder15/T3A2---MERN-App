4aa13c44668d079a1cf4358581ecadcdfc19a493
Mike <michaellaishleys@gmail.com>
2024-08-31 17:06:33 +1000

    added tests for booking route.  Dropdowns and calendar

--- T3A2-B/Frontend/src/tests/LoginField.test.jsx
+++ T3A2-B/Frontend/src/tests/LoginField.test.jsx
@@ -3,17 +3,16 @@ import React, { act } from 'react';
 import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { fireEvent, screen, waitFor } from '@testing-library/react';
 import renderWithRouter from './config/RenderWithRouter';
-import LoginField from '../components/Login/LoginFunc';
+import LoginField from '../components/Login/LoginField';
 import { loginTestUser } from './config/MockSessionState';
 import sessionState from '../routes/store';
-
 
 // Mock react-router-dom to mock useNavigate while keeping other functionality
 vi.mock('react-router-dom', async (importOriginal) => {
   const actual = await importOriginal();
   return {
     ...actual,
-    useNavigate: () => vi.fn(),
+    useNavigate: vi.fn(),
   };
 });
 
@@ -57,12 +56,14 @@ describe('LoginField Component', () => {
   it('should navigate to user account after successful login using loginTestUser', async () => {
     renderWithRouter(<LoginField />);
 
-    // Ensure that loginTestUser is properly awaited and wrapped in act
     await act(async () => {
       await loginTestUser();
     });
 
     await waitFor(() => {
+      const { isAuthenticated } = sessionState.getState();
+      expect(isAuthenticated).toBe(true); // Ensure isAuthenticated is true
+
       expect(screen.getByText('Login Successful!')).toBeInTheDocument();
     });
   });
